634d1d51832e43a9a5813a8dff59af7c0d29267c
Samuel Arao <samuel.arao@gmail.com>
2025-03-07 23:58:03 -0300

    feat: aprendendo loops e aprofundando em funções

---
+++ aula03/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+
+	funcTest := func(param1 string, param2 int)(string){
+		return param1
+	}
+
+	anExpression := false
+	test := []string{"test1","test2","test3"}
+
+	//É um do while
+	for ok := true;ok; ok = anExpression {
+		fmt.Println("Teste 1")
+	}
+
+	//Loop em range
+	for _, value := range test {
+		fmt.Println(value)
+	}
+
+	value, _ := test_func2()
+
+	fmt.Println(value)
+
+	fmt.Println(test_func3(funcTest))
+
+	funcao_retorno := test_func4()
+
+	funcao_retorno("Samuel")
+}
+
+func test_func()(string, error){
+	return "", nil
+}
+
+func test_func2()(retornoString string,retornoErro error){
+	retornoString = "Samuel"
+	retornoErro = nil
+	return
+}
+
+func test_func3(value func(string, int)(string))(retornoString string,retornoErro error){
+	retornoString = value("Otavio", 20)
+	retornoErro = nil
+	return
+}
+
+
+func test_func4() func(string){
+    funcao_retorno := func(valorString string){
+		fmt.Println("É uma string", valorString)
+	}
+	return funcao_retorno
+}
